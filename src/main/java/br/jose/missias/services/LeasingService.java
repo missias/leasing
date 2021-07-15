b2f055ecc2a753efce44827f17c69981933935f2
missias <josecarlosmissias@gmail.com>
2021-07-15 17:55:27 -0300

    Implemented the service class

---
+++ src/main/java/br/jose/missias/services/LeasingService.java
@@ -0,0 +1,33 @@
+package br.jose.missias.services;
+
+import java.util.Date;
+
+import br.jose.missias.entities.Leasing;
+import br.jose.missias.entities.Movie;
+import br.jose.missias.entities.User;
+import br.jose.missias.utils.DateUtils;
+
+public class LeasingService {
+	
+	public Leasing alugarFilme(User user, Movie movie) {
+		Leasing leasing = new Leasing();
+		leasing.setMovie(movie);
+		leasing.setUser(user);
+		leasing.setRentDate(new Date());
+		leasing.setValue(movie.getRentPrice() );
+
+		//Next day delivery
+		Date returnDate = new Date();
+		returnDate = DateUtils.addDays(returnDate, 1);
+		leasing.setRentDate(returnDate);
+		
+		//Saving the location...
+		//TODO add method to save
+		
+		return leasing;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
